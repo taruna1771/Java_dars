652c5b695110a0902c744e67a537ef9d26acbc39
taruna1771 <smannonov70@gmail.com>
2024-03-11 22:33:21 +0500

    O'rganish

---
+++ SpringBoot/Lotus/src/test/java/Birinchi/Lotus/LotusApplicationTests.java
@@ -0,0 +1,13 @@
+package Birinchi.Lotus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LotusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
